01f45c0f22920785141e7364b8b228102c6f7efb
Eshetu Lucas <tekalukas@gmail.com>
2023-12-08 16:02:12 +0300

    Adds open setttings dialog

---
+++ lib/ui/dialogs/open_setting/open_setting_dialog_model.dart
@@ -0,0 +1,10 @@
+import 'package:bnbit_app/app/app.locator.dart';
+import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
+
+class OpenSettingDialogModel extends BaseViewModel {
+  final _navigationService = locator<NavigationService>();
+  void onClosePressed() {
+    _navigationService.back();
+  }
+}
