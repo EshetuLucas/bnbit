f8fe3bd2cd9f95dfc6f46f857f1476f7b93238f3
Eshetu Lucas <tekalukas@gmail.com>
2023-12-08 16:01:46 +0300

    Adds forgot password dialog

---
+++ lib/ui/dialogs/forgot_password/forgot_password_dialog.dart
@@ -0,0 +1,77 @@
+import 'package:bnbit_app/ui/common/app_colors.dart';
+import 'package:bnbit_app/ui/common/shared_styles.dart';
+import 'package:bnbit_app/ui/common/ui_helpers.dart';
+import 'package:bnbit_app/ui/dialogs/basic_dialog.dart';
+import 'package:bnbit_app/ui/widgets/app_button.dart';
+import 'package:bnbit_app/ui/widgets/svg_builder.dart';
+import 'package:bnbit_app/utils/asset_helper.dart';
+import 'package:flutter/material.dart';
+
+import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
+
+import 'forgot_password_dialog_model.dart';
+
+class ForgotPasswordDialog extends StackedView<ForgotPasswordDialogModel> {
+  final DialogRequest request;
+  final Function(DialogResponse) completer;
+
+  const ForgotPasswordDialog({
+    Key? key,
+    required this.request,
+    required this.completer,
+  }) : super(key: key);
+
+  @override
+  Widget builder(
+    BuildContext context,
+    ForgotPasswordDialogModel viewModel,
+    Widget? child,
+  ) {
+    return BasicDialog(
+      onCloseTap: () => Navigator.pop(context),
+      padding: const EdgeInsets.fromLTRB(50, 60, 50, 50),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const SvgBuilder(
+            svg: smileyFace,
+            color: kcWhite,
+            width: 50,
+            height: 30,
+          ),
+          verticalSpace(16),
+          Text(
+            request.title ?? 'Rest password link sent to your email ',
+            textAlign: TextAlign.center,
+            style: ktsHeading900(context).copyWith(
+              fontWeight: FontWeight.w700,
+              color: kcWhite,
+              fontSize: 26,
+            ),
+          ),
+          verticalSpace(11),
+          Text(
+            request.description ??
+                'Please check your email to reset password. And Login again with your new password',
+            textAlign: TextAlign.center,
+            style: ktsSmall(context).copyWith(
+              color: kcWhite,
+              fontSize: 14,
+            ),
+          ),
+          verticalSpace(40),
+          AppButton(
+            title: 'Got it!',
+            onTap: () => Navigator.pop(context),
+          )
+        ],
+      ),
+    );
+  }
+
+  @override
+  ForgotPasswordDialogModel viewModelBuilder(BuildContext context) =>
+      ForgotPasswordDialogModel();
+}
